6bd02c87ee4ac2b8383ebe4b615132e67d1ddcd6
EvandroKayano <ek.kayano@unifesp.br>
2024-07-11 00:41:49 -0300

    6ª vez

--- obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
+++ obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25d3e7d4e653d7cb19e2dd9a5fb2e4693833c608")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beb9bb9704334988d050ee42babfc9a728d308c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
