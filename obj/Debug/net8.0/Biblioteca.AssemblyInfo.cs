9efeff40c1f79793ea298fa9c021a209bebcad3e
Caputino <10758372+capuano1@users.noreply.github.com>
2024-07-14 18:08:16 -0300

    None

--- obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
+++ obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ccbcfbed71c2ea9e05d9e19ebc879182d83454e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f85d8f1ad72bfdbebd48ee460474c8b9b71e2345")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
